manitou-mail/manitou-mail-ui

commit 07674dc75033e849af5e1ca2dea96882a5620561

Add save to disk on attachment's contextual menus and "open with..." with apps associated to file extensions on Windows.

src/msg_list_window.cpp
@@ -310,8 +310,8 @@ msg_list_window::tag_toggled(int tag_id, bool checked)
   uint nsteps = (size+batch_size-1)/batch_size;
   if (nsteps>1) {
     uint processed=0;
-    install_progressbar(this);
-    emit progress(-nsteps);
+    install_progressbar();
+    show_progress(-nsteps);
 
     std::set<mail_msg*> mset;
     for (uint step=0; step < nsteps && !progress_aborted(); step++) {
@@ -325,9 +325,9 @@ msg_list_window::tag_toggled(int tag_id, bool checked)
       if (!mset.empty()) {
 	processed += mail_msg::toggle_tags_set(mset, tag_id, checked);
       }
-      emit progress(1+step);
+      show_progress(1+step);
     }
-    uninstall_progressbar(this);
+    uninstall_progressbar();
     statusBar()->showMessage(tr("%1 message(s) processed.").arg(processed), 3000);
   }
   else for (idx=0; idx < v.size(); idx++) {
@@ -1075,8 +1075,8 @@ msg_list_window::remove_selected_msgs(int action)
   m_ignore_selection_change = true;
   if (nsteps > 1 && action==0) {
     // massive trash, do it in batches
-    install_progressbar(this);
-    emit progress(-nsteps);
+    install_progressbar();
+    show_progress(-nsteps);
 
     std::set<mail_msg*> mset;
     for (uint step=0; step < nsteps && !progress_aborted(); step++) {
@@ -1098,11 +1098,10 @@ msg_list_window::remove_selected_msgs(int action)
 	// auto-select next one only for the last message
 	remove_msg(*it, (cnt==imset.size()));
       }
-      // report progress
-      emit progress(1+step);
+      show_progress(1+step);
     }
     if (nsteps > 1)
-      uninstall_progressbar(this);
+      uninstall_progressbar();
   }
   else for (uint i=0; i<nb_msg; i++) {
     // small update
@@ -1174,8 +1173,8 @@ msg_list_window::change_multi_mail_status(int statusMask,
   statusBar()->showMessage(tr("Updating messages..."));
   uint nsteps = (size+batch_size-1)/batch_size;
   if (nsteps > 1) {
-    install_progressbar(this);
-    emit progress(-nsteps);
+    install_progressbar();
+    show_progress(-nsteps);
   }
 
   DBG_PRINTF(5, "nsteps=%d, size=%d", nsteps, size);
@@ -1200,11 +1199,11 @@ msg_list_window::change_multi_mail_status(int statusMask,
       }
     }
     // report progress
-    emit progress(1+step);
+    show_progress(1+step);
   }
 
   if (nsteps > 1)
-    uninstall_progressbar(this);
+    uninstall_progressbar();
   m_qlist->update();
   QApplication::restoreOverrideCursor();
   statusBar()->showMessage(tr("Done."), 3000);
@@ -1823,6 +1822,36 @@ msg_list_window::enable_interaction(bool b)
 }
 
 void
+msg_list_window::attch_run(attch_lvitem* item)
+{
+  attachment* pa = item->get_attachment();
+  if (!pa) return;
+
+  QString tmpname = pa->get_temp_location();
+  install_progressbar(tr("Downloading attached file: %1").arg(tmpname));
+  item->download(tmpname, &m_abort);
+  if (!m_abort)
+    pa->launch_os_viewer(tmpname);
+  uninstall_progressbar();
+}
+
+/* Returns: 0: cancel, 1: save_file, 2: run default application */
+int
+msg_list_window::attachment_dest(attachment* pa)
+{
+  QString default_app = pa->default_os_application();
+  if (default_app.isEmpty())
+    return 1;
+  attch_dialog_save dlg;
+  dlg.set_app_name(default_app);
+  dlg.set_file_name(pa->filename());
+  int r=dlg.exec();
+  if (r==QDialog::Rejected)
+    return 0;
+  return dlg.choice();
+}
+
+void
 msg_list_window::attch_selected(QTreeWidgetItem* p, int column _UNUSED_)
 {
   attch_lvitem* pItem=dynamic_cast<attch_lvitem*>(p);
@@ -1841,35 +1870,37 @@ msg_list_window::attch_selected(QTreeWidgetItem* p, int column _UNUSED_)
   else {
     if (!pa->application().isEmpty()) {
       // launch helper application (MIME viewer)
-      install_progressbar(m_qAttch);
       QString tmpname = pa->get_temp_location();
-      statusBar()->showMessage(tr("Downloading attached file: %1").arg(tmpname));
+      install_progressbar(tr("Downloading attached file: %1").arg(tmpname));
       pItem->download(tmpname, &m_abort);
       if (!m_abort) {
 	DBG_PRINTF(5,"launch_external_viewer");
 	pa->launch_external_viewer(tmpname);
       }
-      uninstall_progressbar(m_qAttch);
+      uninstall_progressbar();
     }
     else {
-      // save attachment file
-      QString fname = pa->filename();
-      if (m_last_attch_dir.isEmpty()) {
-	m_last_attch_dir = get_config().get_string("attachments_directory");
+      int d=attachment_dest(pa);
+      if (d==2) { // run default app
+	attch_run(pItem);
       }
-      if (!m_last_attch_dir.isEmpty()) {
-	fname = m_last_attch_dir + "/" + fname;
-      }
-//      DBG_PRINTF(5, "fname=%s", fname.latin1());
-      fname = QFileDialog::getSaveFileName(this, tr("Save File"), fname);
-      if (!fname.isEmpty()) {
-	install_progressbar(m_qAttch);
-	statusBar()->showMessage(tr("Downloading attached file: %1").arg(fname));
-	bool m_abort=false;
-	QString dir=pItem->save_to_disk(fname, &m_abort);
-	if (!dir.isEmpty())
-	  m_last_attch_dir = dir;
-	uninstall_progressbar(m_qAttch);
+      if (d==1) { // save to disk
+	QString fname = pa->filename();
+	if (attch_listview::m_last_attch_dir.isEmpty()) {
+	  attch_listview::m_last_attch_dir = get_config().get_string("attachments_directory");
+	}
+	if (!attch_listview::m_last_attch_dir.isEmpty()) {
+	  fname = attch_listview::m_last_attch_dir + "/" + fname;
+	}
+  //      DBG_PRINTF(5, "fname=%s", fname.latin1());
+	fname = QFileDialog::getSaveFileName(this, tr("Save File"), fname);
+	if (!fname.isEmpty()) {
+	  install_progressbar(tr("Downloading attached file: %1").arg(fname));
+	  QString dir=pItem->save_to_disk(fname, &m_abort);
+	  if (!dir.isEmpty())
+	    attch_listview::m_last_attch_dir = dir;
+	  uninstall_progressbar();
+	}
       }
     }
   }
@@ -1882,12 +1913,14 @@ msg_list_window::progress_aborted()
 }
 
 void
-msg_list_window::install_progressbar(QWidget* widget)
+msg_list_window::install_progressbar(QString msg)
 {
   m_new_mail_btn->hide();
   m_progress_bar = new QProgressBar(this);
   statusBar()->addPermanentWidget(m_progress_bar);
-  connect(widget, SIGNAL(progress(int)), this, SLOT(show_progress(int)));
+  if (!msg.isEmpty())
+    statusBar()->showMessage(msg);
+  //  connect(widget, SIGNAL(progress(int)), this, SLOT(show_progress(int)));
   enable_interaction(false);
   show_abort_button();
 }
@@ -1913,11 +1946,10 @@ msg_list_window::hide_abort_button()
 }
 
 void
-msg_list_window::uninstall_progressbar(QWidget* widget)
+msg_list_window::uninstall_progressbar()
 {
   hide_abort_button();
   enable_interaction(true);
-  disconnect(widget, SIGNAL(progress(int)), this, SLOT(show_progress(int)));
   if (m_progress_bar) {
     delete m_progress_bar;
     m_progress_bar=NULL;
@@ -1940,6 +1972,7 @@ msg_list_window::abort_operation()
     unsetCursor();
     statusBar()->showMessage(tr("Query cancelled."));
   }
+  emit abort_progress();
 }
 
 void
@@ -2431,99 +2464,11 @@ msg_list_window::view_attachment()
   qe->show();
 }
 
-bool
-msg_list_window::confirm_write(const QString fname)
-{
-  if (QFile::exists(fname)) {
-    int res = QMessageBox::question(this, QObject::tr("Please confirm"), QObject::tr("A file '%1' already exists. Overwrite?").arg(fname), QObject::tr("&Yes"), QObject::tr("&No"), QString::null, 0, 1);
-    if (res==1)
-      return false;
-  }
-  return true;
-}
 
 void
 msg_list_window::save_attachment()
 {
-  // list of attachments with a filename
-  std::list<attch_lvitem*> list_name;
-  // list of attachments without a filename
-  std::list<attch_lvitem*> list_noname;
-
-  QList<QTreeWidgetItem*> list = m_qAttch->selectedItems();
-  QList<QTreeWidgetItem*>::const_iterator itw = list.begin();
-  for (; itw!=list.end(); ++itw) {
-    attch_lvitem* item = static_cast<attch_lvitem*>(*itw);
-    if (item->is_note())
-      continue;
-    if (!item->get_attachment()->filename().isEmpty())
-      list_name.push_back(item);
-    else
-      list_noname.push_back(item);
-  }
-  if (list_name.empty() && list_noname.empty()) {
-    QMessageBox::warning(this, APP_NAME, tr("Please select one or several attachments"));
-    return;
-  }
-
-  QString fname;
-  QString dir=m_last_attch_dir;
-  if (dir.isEmpty()) {
-    dir = get_config().get_string("attachments_directory");
-  }
-  std::list<attch_lvitem*>::iterator it;
-  if (list_name.size()>1) {
-    // several named attachments: ask only for the directory
-    dir = QFileDialog::getExistingDirectory(this, tr("Save to directory..."), dir);
-    if (!dir.isEmpty()) { // if accepted
-      for (it=list_name.begin(); it!=list_name.end(); ++it) {
-	attachment* pa = (*it)->get_attachment();
-	fname = dir + "/" + pa->filename();
-	if (!confirm_write(fname))
-	  continue;
-	install_progressbar(m_qAttch);
-	//DBG_PRINTF(6, "downloading %s", fname.latin1());
-	statusBar()->showMessage(tr("Downloading attachment into: %1").arg(fname));
-	dir=(*it)->save_to_disk(fname, &m_abort);
-	uninstall_progressbar(m_qAttch);
-	if (m_abort)
-	  break;
-      }
-    }
-    else
-      return;			// cancel
-  }
-  if (list_name.size()==1) {
-    it=list_name.begin();
-    attachment* pa = (*it)->get_attachment();
-    fname = pa->filename();
-    if (!dir.isEmpty()) {
-      fname = dir + "/" + fname;
-    }
-    //DBG_PRINTF(5, "fname=%s", fname.latin1());
-    fname = QFileDialog::getSaveFileName(this, tr("File"), fname);
-    if (!fname.isEmpty()) {
-      install_progressbar(m_qAttch);
-      statusBar()->showMessage(tr("Downloading attached file: %1").arg(fname));
-      dir=(*it)->save_to_disk(fname, &m_abort);
-      uninstall_progressbar(m_qAttch);
-    }
-  }
-  if (!list_noname.empty()) {
-    for (it=list_noname.begin(); it!=list_noname.end(); ++it) {
-      fname = QFileDialog::getSaveFileName(this, tr("File"), dir);
-      if (!fname.isEmpty()) {
-	install_progressbar(m_qAttch);
-	statusBar()->showMessage(tr("Downloading attachment into: %1").arg(fname));
-	dir=(*it)->save_to_disk(fname, &m_abort);
-	uninstall_progressbar(m_qAttch);
-      }
-      else
-	break;
-    }
-  }
-  if (!dir.isEmpty())
-    m_last_attch_dir=dir;
+  m_qAttch->save_attachments();
 }
 
 // slot

src/attachment.cpp
@@ -35,6 +35,8 @@
 
 #ifdef Q_OS_WIN
 #include <windows.h>
+#include <shlwapi.h>
+#include <tchar.h>
 #endif
 
 attachment::attachment() :
@@ -70,6 +72,26 @@ attachment::is_binary()
       return true;
   }
   return false;
+}
+
+QString
+attachment::default_os_application()
+{
+#ifdef Q_OS_WIN
+  QString ext = extension(filename());
+  if (!ext.isEmpty()) {
+    ext.prepend(".");
+    DWORD dwSize = 255;
+    TCHAR sBuffer[MAX_PATH] = {0};
+    wchar_t* exta = new wchar_t[ext.length()+1];
+    ext.toWCharArray(exta);
+    exta[ext.length()]='\0';
+    HRESULT hr = AssocQueryString((ASSOCF)0, ASSOCSTR_FRIENDLYAPPNAME, exta, L"Open", sBuffer, &dwSize);
+    delete exta;
+    return QString::fromWCharArray(sBuffer);
+  }
+#endif
+  return QString::null;
 }
 
 QString
@@ -184,8 +206,30 @@ attachment::get_temp_location()
     fname = QString("attch-%1-%2").arg(m_Id).arg(m_filename);
   }
   QString dirname=get_config().get_string("attachments_directory");
-  QDir dir = dirname.isEmpty() ? QDir::temp() : QDir(dirname);
+  QDir dir;
+  if (!dirname.isEmpty()) {
+    dir = QDir(dirname);
+    if (!dir.exists())
+      dir = QDir::temp();
+  }
+  else
+    dir = QDir::temp();
   return dir.absoluteFilePath(fname);
+}
+
+void
+attachment::launch_os_viewer(const QString document_path)
+{
+#ifdef Q_OS_WIN
+  SHELLEXECUTEINFO shellInfo;
+  memset(&shellInfo, 0, sizeof(SHELLEXECUTEINFO));
+  shellInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+  shellInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+  shellInfo.lpFile = (LPCTSTR)document_path.utf16();
+  shellInfo.lpParameters = NULL;
+  shellInfo.nShow = SW_SHOW;
+  ::ShellExecuteEx(&shellInfo);
+#endif
 }
 
 void
@@ -201,14 +245,7 @@ attachment::launch_external_viewer(const QString document_path)
 	 attachment's name itself is passed to ShellExecute. It assumes
 	 that it triggers the association at the OS level between the file
 	 extension and a program */
-      SHELLEXECUTEINFO shellInfo;
-      memset(&shellInfo, 0, sizeof(SHELLEXECUTEINFO));
-      shellInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-      shellInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-      shellInfo.lpFile = (LPCTSTR)document_path.utf16();
-      shellInfo.lpParameters = NULL;
-      shellInfo.nShow = SW_SHOW;
-      ::ShellExecuteEx(&shellInfo);
+      launch_os_viewer(document_path);
       return;
     }
 #endif
@@ -509,14 +546,24 @@ attachment::guess_mime_type(const QString filename)
     fetch_filename_suffixes(extension_map);
   }
 
-  int dotpos = filename.lastIndexOf('.');
-  if (dotpos >=0 && dotpos+1 < filename.length()) {
-    QString ext = filename.mid(dotpos+1);
+  QString ext = extension(filename);
+  if (!ext.isEmpty()) {
     mt::const_iterator it = extension_map.constFind(ext);
     if (it != extension_map.constEnd())
       return it.value();
   }
   return QString("application/octet-stream");	// nothing found
+}
+
+// static
+QString
+attachment::extension(const QString filename)
+{
+  int dotpos = filename.lastIndexOf('.');
+  if (dotpos >=0 && dotpos+1 < filename.length())
+    return filename.mid(dotpos+1);
+  else
+    return QString::null;
 }
 
 void

src/attachment.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004-2011 Daniel Verite
+/* Copyright (C) 2004-2012 Daniel Verite
 
    This file is part of Manitou-Mail (see http://www.manitou-mail.org)
 
@@ -68,10 +68,13 @@ public:
   static QString guess_mime_type(const QString);
   uint size() { fetch(); return m_size; }
   const QString filename() { fetch(); return m_filename; }
+  static QString extension(const QString);
   QString mime_type() { fetch(); return m_mime_type; }
   QString charset() { fetch(); return m_charset; }
   QString application() const;
+  QString default_os_application();
   void launch_external_viewer(const QString document_path);
+  void launch_os_viewer(const QString document_path);
 
   attachment_network_reply* network_reply(const QNetworkRequest& req, QObject* parent);
 

src/attachment_listview.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004-2010 Daniel Verite
+/* Copyright (C) 2004-2012 Daniel Verite
 
    This file is part of Manitou-Mail (see http://www.manitou-mail.org)
 
@@ -27,6 +27,14 @@
 #include <QMenu>
 #include <QPlainTextEdit>
 #include <QUrl>
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QBoxLayout>
+#include <QLabel>
+#include <QRadioButton>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QMessageBox>
 
 // *************** //
 //  attch_lvitem
@@ -137,6 +145,8 @@ attch_listview::~attch_listview()
 {
 }
 
+QString attch_listview::m_last_attch_dir;
+
 void
 attch_listview::popup_ctxt_menu(const QPoint& pos)
 {
@@ -147,7 +157,12 @@ attch_listview::popup_ctxt_menu(const QPoint& pos)
   if (!pa)
     return; // it may be a note
 
+  QString app_name = pa->default_os_application();
   QMenu menu(this);
+  QAction* action_open_app = NULL;
+  if (!app_name.isEmpty())
+    action_open_app = menu.addAction(tr("Open with: %1").arg(app_name));
+  QAction* action_save = menu.addAction(tr("Save to disk"));
   QAction* action_view_text = menu.addAction(tr("Display as text"));
 
   QAction* action = menu.exec(mapToGlobal(pos));
@@ -164,6 +179,24 @@ attch_listview::popup_ctxt_menu(const QPoint& pos)
       v->show();
     }
   }
+  else if (action==action_open_app) {
+    QString tmpname = pa->get_temp_location();
+    emit init_progress(tr("Downloading attached file: %1").arg(tmpname));
+    m_abort=false;
+    item->download(tmpname, &m_abort);
+    emit finish_progress();
+    if (!m_abort)
+      pa->launch_os_viewer(tmpname);
+  }
+  else if (action==action_save) {
+    save_attachments();
+  }
+}
+
+void
+attch_listview::download_aborted()
+{
+  m_abort=true;
 }
 
 void
@@ -226,3 +259,133 @@ attch_listview::mimeTypes() const
   return QStringList("text/uri-list");
 }
 
+bool
+attch_listview::confirm_write(const QString fname)
+{
+  if (QFile::exists(fname)) {
+    int res = QMessageBox::question(this, QObject::tr("Please confirm"), QObject::tr("A file '%1' already exists. Overwrite?").arg(fname), QObject::tr("&Yes"), QObject::tr("&No"), QString::null, 0, 1);
+    if (res==1)
+      return false;
+  }
+  return true;
+}
+
+
+void
+attch_listview::save_attachments()
+{
+  // list of attachments with a filename
+  std::list<attch_lvitem*> list_name;
+  // list of attachments without a filename
+  std::list<attch_lvitem*> list_noname;
+
+  QList<QTreeWidgetItem*> list = this->selectedItems();
+  QList<QTreeWidgetItem*>::const_iterator itw = list.begin();
+  for (; itw!=list.end(); ++itw) {
+    attch_lvitem* item = static_cast<attch_lvitem*>(*itw);
+    if (item->is_note())
+      continue;
+    if (!item->get_attachment()->filename().isEmpty())
+      list_name.push_back(item);
+    else
+      list_noname.push_back(item);
+  }
+  if (list_name.empty() && list_noname.empty()) {
+    QMessageBox::warning(this, APP_NAME, tr("Please select one or several attachments"));
+    return;
+  }
+
+  QString fname;
+  QString dir=attch_listview::m_last_attch_dir;
+  if (dir.isEmpty()) {
+    dir = get_config().get_string("attachments_directory");
+  }
+  std::list<attch_lvitem*>::iterator it;
+  if (list_name.size()>1) {
+    // several named attachments: ask only for the directory
+    dir = QFileDialog::getExistingDirectory(this, tr("Save to directory..."), dir);
+    if (!dir.isEmpty()) { // if accepted
+      for (it=list_name.begin(); it!=list_name.end(); ++it) {
+	attachment* pa = (*it)->get_attachment();
+	fname = dir + "/" + pa->filename();
+	if (!confirm_write(fname))
+	  continue;
+	emit init_progress(tr("Downloading attachment into: %1").arg(fname));
+	dir=(*it)->save_to_disk(fname, &m_abort);
+	emit finish_progress();
+	if (m_abort)
+	  break;
+      }
+    }
+    else
+      return;			// cancel
+  }
+  if (list_name.size()==1) {
+    it=list_name.begin();
+    attachment* pa = (*it)->get_attachment();
+    fname = pa->filename();
+    if (!dir.isEmpty()) {
+      fname = dir + "/" + fname;
+    }
+    DBG_PRINTF(5, "fname=%s", fname.toLocal8Bit().constData());
+    fname = QFileDialog::getSaveFileName(this, tr("File"), fname);
+    if (!fname.isEmpty()) {
+      emit init_progress(tr("Downloading attached file: %1").arg(fname));
+      dir=(*it)->save_to_disk(fname, &m_abort);
+      emit finish_progress();
+    }
+  }
+  if (!list_noname.empty()) {
+    for (it=list_noname.begin(); it!=list_noname.end(); ++it) {
+      fname = QFileDialog::getSaveFileName(this, tr("File"), dir);
+      if (!fname.isEmpty()) {
+	emit init_progress(tr("Downloading attachment into: %1").arg(fname));
+	dir=(*it)->save_to_disk(fname, &m_abort);
+	emit finish_progress();
+      }
+      else
+	break;
+    }
+  }
+  if (!dir.isEmpty())
+    attch_listview::m_last_attch_dir=dir;
+}
+
+attch_dialog_save::attch_dialog_save()
+{
+  QVBoxLayout* layout = new QVBoxLayout(this);
+  layout->setMargin(5);
+  setWindowTitle(tr("Open or save"));
+  m_label_file = new QLabel;
+  m_label_file->setTextFormat(Qt::PlainText);
+  layout->addWidget(m_label_file);
+  m_rb_launch = new QRadioButton(tr("Open with: %1").arg(""));
+  QRadioButton* b2 = new QRadioButton(tr("Save to disk"));
+  b2->setChecked(true);
+  layout->addWidget(m_rb_launch);
+  layout->addWidget(b2);
+  QDialogButtonBox* box = new QDialogButtonBox;
+  layout->addWidget(box);
+  box->addButton(tr("OK"), QDialogButtonBox::AcceptRole);
+  box->addButton(tr("Cancel"), QDialogButtonBox::RejectRole);
+  connect(box, SIGNAL(accepted()), this, SLOT(accept()));
+  connect(box, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
+void
+attch_dialog_save::set_app_name(const QString name)
+{
+  m_rb_launch->setText(tr("Open with: %1").arg(name));
+}
+
+void
+attch_dialog_save::set_file_name(const QString name)
+{
+  m_label_file->setText(tr("File: %1").arg(name));
+}
+
+int
+attch_dialog_save::choice()
+{
+  return (m_rb_launch->isChecked()) ? 2 : 1;
+}

src/attachment_listview.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004-2010 Daniel Verite
+/* Copyright (C) 2004-2012 Daniel Verite
 
    This file is part of Manitou-Mail (see http://www.manitou-mail.org)
 
@@ -22,9 +22,12 @@
 
 #include <QPalette>
 #include <QTreeWidget>
+#include <QDialog>
 #include "attachment.h"
 
 class QMimeData;
+class QRadioButton;
+class QLabel;
 
 class attch_listview: public QTreeWidget
 {
@@ -37,6 +40,7 @@ public:
   }
   void progress_report(int);
   void allow_delete(bool b);
+  void save_attachments();
 protected:
   bool dropMimeData(QTreeWidgetItem *parent, int index, const QMimeData *data, Qt::DropAction action);
   Qt::DropActions supportedDropActions() const;
@@ -45,12 +49,24 @@ protected:
 private:
   // ptr to the list of attachments of the message object
   attachments_list* m_pAttchList;
+  bool m_abort; //  attachment download aborted
 signals:
   void progress(int);
-  void attach_file_request(const QUrl);  
+  void attach_file_request(const QUrl);
+  void init_progress(const QString);
+  void finish_progress();
+public slots:
+  void download_aborted();
 private slots:
   void remove_current_attachment();
   void popup_ctxt_menu(const QPoint&);
+
+  // check if fname exists and if it does ask whether it should
+  // be overwritten or not
+  bool confirm_write(const QString fname);
+
+public:
+  static QString m_last_attch_dir;
 };
 
 class attch_lvitem : public QTreeWidgetItem
@@ -96,4 +112,18 @@ private:
   QPalette m_palette_for_notes;
   attch_listview* m_listview;
 };
+
+class attch_dialog_save: public QDialog
+{
+  Q_OBJECT
+public:
+  attch_dialog_save();
+  void set_app_name(const QString);
+  void set_file_name(const QString);
+  int choice();
+private:
+  QRadioButton* m_rb_launch;
+  QLabel* m_label_file;
+};
+
 #endif

src/msg_list_window.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004-2011 Daniel Verite
+/* Copyright (C) 2004-2012 Daniel Verite
 
    This file is part of Manitou-Mail (see http://www.manitou-mail.org)
 
@@ -55,6 +55,8 @@ class query_listview;
 class mail_listview;
 class attch_listview;
 class message_view;
+class attch_lvitem;
+class attachment;
 
 class newmail_button: public QPushButton
 {
@@ -143,6 +145,8 @@ public slots:
   void timer_func();
   void timer_idle();
   void abort_operation();
+  void install_progressbar(QString=QString::null);
+  void uninstall_progressbar();
 
   void msg_zoom_in();
   void msg_zoom_out();
@@ -212,6 +216,8 @@ public slots:
   void action_click_msg_list(const QModelIndex& index);
   void bounce();
   void attch_selected(QTreeWidgetItem*,int);
+  void attch_run(attch_lvitem*);
+  int attachment_dest(attachment*);
   void find_text();
   void tag_toggled(int id, bool checked);
 
@@ -265,17 +271,11 @@ private:
   // reflect state of abort button during a progress-controlled operation
   bool progress_aborted();
 
-  // check if fname exists and if it does ask whether it should
-  // be overwritten or not
-  bool confirm_write(const QString fname);
-
   /* enable/disable menus and widgets. use to avoid recurse
      into the gui when an action is performed during
      which Qt event loop may be called */
   void enable_interaction(bool b);
 
-  void install_progressbar(QWidget*);
-  void uninstall_progressbar(QWidget*);
   void show_abort_button();
   void hide_abort_button();
 
@@ -421,8 +421,6 @@ private:
   bool m_highlightedCaseSensitive;
   search_box* m_wSearch;
 
-  QString m_last_attch_dir;
-
   bool m_fetch_on_demand;
   bool m_ignore_selection_change;
 
@@ -468,6 +466,7 @@ signals:
   void mail_chg_status(int, mail_msg*);
   void mail_multi_chg_status(int, std::vector<mail_msg*>*);
   void progress(int);
+  void abort_progress();
 };
 
 #endif // INC_MSG_LIST_WINDOW_H

src/msg_list_window_pages.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004-2010 Daniel Verite
+/* Copyright (C) 2004-2012 Daniel Verite
 
    This file is part of Manitou-Mail (see http://www.manitou-mail.org)
 
@@ -210,6 +210,11 @@ msg_list_window::add_msgs_page(const msgs_filter* f, bool if_results _UNUSED_)
   m_qAttch = new attch_listview(l);
   connect(m_qAttch, SIGNAL(itemDoubleClicked(QTreeWidgetItem*,int)),
 	  this, SLOT(attch_selected(QTreeWidgetItem*,int)));
+  connect(m_qAttch, SIGNAL(init_progress(const QString)),
+	  this, SLOT(install_progressbar(const QString)));
+  connect(m_qAttch, SIGNAL(progress(int)), this, SLOT(show_progress(int)));
+  connect(m_qAttch, SIGNAL(finish_progress()), this, SLOT(uninstall_progressbar()));
+  connect(this, SIGNAL(abort_progress()), m_qAttch, SLOT(download_aborted()));
 
   // splitter for 3 panes: list of messages / body / attachments list
   static const int splitter_default_sizes[3] = {100,400,25};

commit 6664944d09a46e9360b6eb242a21f26bbfa64ae9

Fix bug introduced by 07674dc: don't update an uninitialized progressbar.

src/msg_list_window.cpp
@@ -1172,7 +1172,7 @@ msg_list_window::change_multi_mail_status(int statusMask,
 
   statusBar()->showMessage(tr("Updating messages..."));
   uint nsteps = (size+batch_size-1)/batch_size;
-  if (nsteps > 1) {
+  if (nsteps>1) {
     install_progressbar();
     show_progress(-nsteps);
   }
@@ -1199,10 +1199,11 @@ msg_list_window::change_multi_mail_status(int statusMask,
       }
     }
     // report progress
-    show_progress(1+step);
+    if (nsteps>1)
+      show_progress(1+step);
   }
 
-  if (nsteps > 1)
+  if (nsteps>1)
     uninstall_progressbar();
   m_qlist->update();
   QApplication::restoreOverrideCursor();
@@ -2476,6 +2477,10 @@ void
 msg_list_window::show_progress(int progress)
 {
   DBG_PRINTF(5, "show_progress(%d)", progress);
+  if (!m_progress_bar) {
+    DBG_PRINTF(2, "show_progress: no progress bar!");
+    return;
+  }
   // when negative, progress is the total number of steps expected
   if (progress<0) {
     m_progress_bar->setMaximum(-progress);